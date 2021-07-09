9f61b8b9638be660b446f234c54179d269a0a569
rinLyn <rintaro_fujii@posiwill.co.jp>
2021-07-09 16:37:48 +0900

    yay

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,12 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                    <img src="{{ asset('/assets/images/plants8.jpeg') }}" style="max-width: 100%;">
+                    <div>
+                        <p class="mt-5">人気の投稿</p>
+                    </div>
                 </div>
             </div>
         </div>
